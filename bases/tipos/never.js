383991cff0a7cf0ad8b7916f9deb4f0be298541c
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-11 18:03:23 -0300

    Never

---
+++ bases/tipos/never.js
@@ -0,0 +1,10 @@
+(() => {
+    const error = (message) => {
+        if (false) {
+            throw new Error(message);
+        }
+        return 1;
+    };
+    error('Auxilio!');
+})();
+export {};
