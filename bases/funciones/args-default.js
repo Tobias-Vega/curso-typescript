8db590fe926cafccede3a2016c6756c7f22de2dc
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-11 22:39:46 -0300

    Parámetros por defecto

---
+++ bases/funciones/args-default.js
@@ -0,0 +1,11 @@
+(() => {
+    const fullName = (firstName, lastName, upper = false) => {
+        if (upper) {
+            return `${firstName} ${lastName || '------'}`.toUpperCase();
+        }
+        return `${firstName} ${lastName || '------'}`;
+    };
+    const name = fullName('Tony', 'Stark', true);
+    console.log({ name });
+})();
+export {};
