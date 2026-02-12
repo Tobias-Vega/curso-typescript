38c8f37458cf18bf9604a90c7805cf592463d697
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-11 23:09:18 -0300

    Resolución del ejercicio práctico

--- bases/app.ts
+++ bases/app.ts
@@ -1,45 +1,32 @@
-(() => {
+// Funciones Básicas
+const sumar = ( a: number, b: number ): number => {
+  return a + b;
+}
 
-    // Tipos
-    const batman: string = 'Bruce';
-    const superman: string = 'Clark';
-  
-    const existe: boolean = false;
-  
-    // Tuplas
-    const parejaHeroes: [string, string] = [batman,superman];
-    const villano: [string,number,boolean] = ['Lex Lutor',5,true];
-  
-    // Arreglos
-    const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
-  
-    //Enumeraciones
-    enum PowerHeroes  {
-      acuaman = 0,
-      batman = 1,
-      flash = 5,
-      superman = 100,
-    }
-    const fuerzaFlash: PowerHeroes = PowerHeroes.flash;
-    const fuerzaSuperman: PowerHeroes = PowerHeroes.superman;
-    const fuerzaBatman: PowerHeroes = PowerHeroes.flash;
-    const fuerzaAcuaman: PowerHeroes = PowerHeroes.acuaman;
-  
-    // Retorno de funciones
-    function activar_batiseñal(): string {
-      return 'activada';
-    }
-  
-    function pedir_ayuda(): void {
-      console.log('Auxilio!!!');
-    }
-  
-    // Aserciones de Tipo
-    const poder: any = '100';
-    const largoDelPoder:number = (poder as string).length;
-    console.log( largoDelPoder );
-  
-  
-  })()
-  
-  
+const contar = ( heroes: string[] ): number => {
+  return heroes.length;
+}
+const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+contar(superHeroes);
+
+//Parametros por defecto
+const llamarBatman = ( llamar = true ): void => {
+  if( llamar ){
+    console.log("Batiseñal activada");
+  }
+}
+
+llamarBatman();
+
+// Rest?
+const unirheroes = ( ...personas: string[] ): string => {
+  return personas.join(", ");
+}
+
+
+// Tipo funcion
+const noHaceNada = ( numero: number, texto: string, booleano: boolean, arreglo: string[] )=> {}
+
+// Crear el tipo de funcion que acepte la funcion "noHaceNada"
+let noHaceNadaTampoco: (a:number, b: string, c: boolean, d: string[] ) => void;
+noHaceNadaTampoco = noHaceNada
