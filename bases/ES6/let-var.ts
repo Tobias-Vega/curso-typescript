a40280c7ea3d6da2a5b4b5cb4541d26653311c57
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 15:51:58 -0300

    Inicio Sección 7 - Variables let

---
+++ bases/ES6/let-var.ts
@@ -0,0 +1,17 @@
+(() => {
+  console.log('let');
+
+  // console.log(a);
+
+  const nombre: string = 'Tobías';
+
+  const getName = (): void => {
+    console.log('viejo getName');
+
+
+  }
+
+  // getName = () => { console.log('Nuevo getName') }
+  // getName()
+
+})
