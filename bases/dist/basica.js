d1f9a6afe9cf749cf30d7d92cc49e2db14abd27e
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 17:00:19 -0300

    Clases en TS - Inicio Sección 8

---
+++ bases/dist/basica.js
@@ -0,0 +1,15 @@
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+        }
+    }
+    Avenger.avgAge = 35;
+    const antman = new Avenger('Antman', 'Capitan');
+    console.log(antman);
+    console.log(Avenger.avgAge);
+})();
+export {};
+//# sourceMappingURL=basica.js.map
