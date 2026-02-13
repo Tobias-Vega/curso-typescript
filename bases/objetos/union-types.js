ecc17d9d84c6c2f8b277ad760b3c0715e0f70f07
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 14:36:33 -0300

    Union types

---
+++ bases/objetos/union-types.js
@@ -0,0 +1,13 @@
+(() => {
+    let myCustomVariable = 'Tobías';
+    console.log(typeof myCustomVariable);
+    myCustomVariable = 20;
+    console.log(typeof myCustomVariable);
+    myCustomVariable = {
+        name: 'Bruce',
+        age: 43,
+        powers: [1],
+    };
+    console.log(typeof myCustomVariable);
+})();
+export {};
