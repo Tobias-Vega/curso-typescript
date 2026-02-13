75c0fc7f6550548cee1c2c63a059e5910fe4df85
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 14:04:58 -0300

    Inicio sección 5 - Objetos básicos

---
+++ bases/objetos/object.ts
@@ -0,0 +1,19 @@
+(() => {
+
+
+  let flash = {
+    name: 'Barry Allen',
+    age: 24,
+    powers: ['Super velocidad', 'Viajar en el tiempo'],
+  }
+
+  flash = {
+    name: 'Clack Kent',
+    age: 60,
+    powers: ['Super fuerza'],
+    getNombre() {
+      return this.name
+    }
+  }
+
+})();
