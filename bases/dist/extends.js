bdf33fb6d7effb514c6da9c9307accefdfdcebfa
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 17:16:17 -0300

    Herencia, super y extends

---
+++ bases/dist/extends.js
@@ -0,0 +1,26 @@
+(() => {
+    class Avenger {
+        constructor(name, realName) {
+            this.name = name;
+            this.realName = realName;
+            console.log('Constructor Avenger llamado');
+        }
+        getFullName() {
+            return `${this.name} ${this.realName}`;
+        }
+    }
+    class Xmen extends Avenger {
+        constructor(name, realName, isMutant) {
+            super(name, realName);
+            this.isMutant = isMutant;
+            console.log('Constructor Xmen llamado');
+        }
+        getFullNameDesdeXmen() {
+            console.log(super.getFullName());
+        }
+    }
+    const wolverine = new Xmen('Wolverine', 'Logan', true);
+    console.log(wolverine.getFullNameDesdeXmen());
+})();
+export {};
+//# sourceMappingURL=extends.js.map
