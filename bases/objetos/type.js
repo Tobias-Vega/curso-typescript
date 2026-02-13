6f656cb75b1c5f243a3c8f6900955bf6b1ac94c5
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 14:29:00 -0300

    types personalizados

---
+++ bases/objetos/type.js
@@ -0,0 +1,16 @@
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: [1, 2],
+    };
+    let superman = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: [1],
+        getName() {
+            return this.name;
+        },
+    };
+})();
+export {};
