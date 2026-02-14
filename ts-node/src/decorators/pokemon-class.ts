1878b68cbbd2138e8fc17de81eb213c0891c0c2f
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 23:15:59 -0300

    Factory decorators

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -2,8 +2,17 @@ function printToConsole(constructor: Function) {
   console.log(constructor);
 }
 
+const printToConsoleConditional = (print: boolean = false): Function => {
 
-@printToConsole
+  if (print) {
+    return printToConsole;
+  } else {
+    return () => {}
+  }
+}
+
+
+@printToConsoleConditional(false)
 export class Pokemon {
 
   public publicApi: string = 'https://pokeapi.co';
