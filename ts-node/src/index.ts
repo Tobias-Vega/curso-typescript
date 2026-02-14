428c4d3656e21ee0349194515e5367098bd3e091
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 23:10:37 -0300

    Decorador de clases

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,7 +1,4 @@
-import { getPokemon } from "./generics/get-pokemon";
+import { Pokemon } from "./decorators/pokemon-class";
 
 
-getPokemon(4)
-  .then(pokemon => console.log(pokemon.sprites.front_default))
-  .catch(error => console.log(error))
-  .finally(() => console.log('Fin del getPokemon'))
+const charmander = new Pokemon ('Charmander');
