14e5861fc4a830e1ce3664f215cd5485b9111723
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 21:20:28 -0300

    Exportación con alias y default

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,6 +1,11 @@
-import { Hero } from "./classes/Hero";
+import * as HeroClasses from "./classes/Hero";
+import powers, {Power} from "./data/powers";
 
+const Hero = 123;
 
-const ironman = new Hero('Ironman', 1, 55);
+// const ironman = new SuperHero('Ironman', 1, 55);
+const ironman = new HeroClasses.Hero('Ironman', 1, 55);
 
 console.log(ironman);
+
+console.log(powers);
