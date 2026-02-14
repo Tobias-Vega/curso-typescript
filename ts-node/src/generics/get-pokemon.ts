c64034fd7b2e805255f16da6965a8fbda5f20617
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 22:22:07 -0300

    Mapear respuestas HTTP

--- ts-node/src/generics/get-pokemon.ts
+++ ts-node/src/generics/get-pokemon.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
+import { Pokemon } from "../interfaces";
 
 
 
-export const getPokemon = async (pokemonId: number) => {
+export const getPokemon = async (pokemonId: number): Promise<Pokemon> => {
 
-  const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+  const resp = await axios.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
 
-  console.log(resp);
+  console.log(resp.data);
 
-  return 1;
+  return resp.data;
 }
