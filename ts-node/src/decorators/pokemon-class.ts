36d478b16843cdba58ebdbc807e1e5a2bc54a220
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-13 23:31:51 -0300

    Decoradores de métodos

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -16,14 +16,33 @@ const bloquearPrototipo = function(constructor: Function) {
   Object.seal(constructor.prototype);
 }
 
+function CheckValidPokemonId() {
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+
+    const originalMethod = descriptor.value;
+    descriptor.value = (id: number) => {
+      if (id < 1 || id > 800) {
+        return console.error('El id del pokemón debe de estar entre 1 y 800');
+      } else {
+        return originalMethod(id)
+      }
+    }
+
+    // descriptor.value = () => console.log('Hola mundo');
+  }
+}
+
 @bloquearPrototipo
-@printToConsoleConditional(true)
+@printToConsoleConditional(false)
 export class Pokemon {
 
   public publicApi: string = 'https://pokeapi.co';
   constructor(
     public name: string,
-  ) {
+  ) {}
 
+  @CheckValidPokemonId()
+  savePokemonToDB(id: number) {
+    console.log(`Pokemon guardado en DB ${id}`);
   }
 }
